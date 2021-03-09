ce47e750d24a80aae2483a2772f755bcf3382b46
yousef-ghanim <yousefghanim85@gmail.com>
2021-03-09 09:50:53 +0000

    fixing format

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
   const [color, setColor] = useState("");
@@ -9,36 +9,47 @@ function App() {
   const [list, setList] = useState(new Values("#f15025").all(10));
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      let colors = new Values(color).all(10)
-      setList(colors)
+      let colors = new Values(color).all(10);
+      setList(colors);
     } catch (error) {
-      setError(true)
-      console.log(error)
+      setError(true);
+      console.log(error);
     }
-    
-  }
+  };
   return (
     <>
-  <section className="container">
-    <h3>color generator</h3>
-    <form onSubmit={handleSubmit}>
-    <input type="text" value={color} onChange={(e)=> setColor(e.target.value)}
-    placeholder="#f15025"
-    className={`${error ? "error" : null}`}
-    />
-    <button className="btn" type="submit">submit</button>
-    </form>
-  </section>
-  <section className="colors">
-    {list.map((color, index) =>{
-      console.log(color)
-      return <SingleColor key={index} {...color} index={index} hexColor={color.hex} />
-    })}
-  </section>
-  </>
-  )
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          console.log(color);
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
